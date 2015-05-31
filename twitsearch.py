abfdf495ab30aebc80e6a599745e8b13910cbbbd
shetimaj <shetimaj@users.noreply.github.com>
2015-05-31 13:48:49 -0400

    Update twitsearch.py

--- twitsearch.py
+++ twitsearch.py
@@ -9,10 +9,10 @@ from HTMLParser import HTMLParser
 import csv
 
 
-ckey = 'xHPB5wMHmxP5KNzV2brbdfCp6' #private
-csecret = 'F7qCZ5Vrm0mzZs5IERziZAcRLygV5vqolGlhWkdGZswjGeghbA'
-atoken = '185044419-nu83wmFcwdrZHwqhZDrIWMSeprJhsyHC82Ao2vJQ'
-asecret = '368hMVnP7AnNVnvsfHyuCQNxSjnZgYKvu3HAk37WLc875'
+ckey = 'xxxxx' #private
+csecret = 'xxxxx'
+atoken = 'xxxxxx'
+asecret = 'xxxxxx'
 
 #Create map template to display tweet locations
 m = Basemap(projection='merc', llcrnrlat=30, urcrnrlat=50,\
